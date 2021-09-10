98c1600955a60924f94d6d3c5dd6e115e4d4457a
sattama <satadhitama@gmail.com>
2021-09-10 11:36:09 +0700

    Adding database model

---
+++ datastore/mongo/model/booking.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  id: {
+    type: String,
+    require: true
+  },
+  userId: {
+    type: String,
+    require: true,
+  },
+  eventId: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: String,
+    require: true,
+  }
+})
+
+module.exports = mongoose.model('Bookings', schema, 'bookings')
---
+++ datastore/mongo/model/event.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  id: {
+    type: String,
+    require: true
+  },
+  title: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    require: true,
+  },
+  start: {
+    type: Date,
+    require: true,
+  },
+  end: {
+    type: Date,
+    require: true,
+  },
+  capacity: {
+    type: Number,
+    require: true,
+  },
+  enrolled: {
+    type: Number,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  link: {
+    type: String,
+    require: true,
+  },
+  imagesId: {
+    type: String,
+    require: true,
+  },
+  alumniId: {
+    type: String,
+    require: true
+  },
+  createdAt: {
+    type: Date,
+    require: true
+  },
+  updatedAt: {
+    type: Date,
+    require: true
+  }
+})
+
+module.exports = mongoose.model('Events', schema, 'events')
